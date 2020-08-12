a2ec4d3787ba03747bf215c9ce3ecf10180d7e40
5kft <5kft@users.noreply.github.com>
2020-08-12 07:58:12 -0700

    Add support for the Exen Mini

---
+++ boards/exen-mini/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME	"Nerdonic"
+#define PRODUCT_NAME	"Exen Mini"
+#define VOLUME_LABEL	"EXEN-MINI"
+#define INDEX_URL	"https://nerdonic.com/products/exen/mini"
+#define BOARD_ID	"SAMD21G18A-Exen-Mini-v0"
+
+#define USB_VID		0x1B4F
+#define USB_PID		0x0D4E
+
+#define LED_PIN PIN_PA06
+
+#endif
